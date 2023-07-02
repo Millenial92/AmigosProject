763255ef83ea1ae98befd42d107f3400c6a5d869
Gitdemo <007sauravfrenzy@gmail.com>
2023-07-02 22:55:39 +0530

    AmigosCommit

---
+++ demo/src/main/java/com/example/amigos/demo/student/Gender.java
@@ -0,0 +1,7 @@
+package com.example.amigos.demo.student;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHERS
+}
---
+++ demo/src/main/java/com/example/amigos/demo/student/Student.java
@@ -0,0 +1,17 @@
+package com.example.amigos.demo.student;
+
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private Gender gender;
+}
+
